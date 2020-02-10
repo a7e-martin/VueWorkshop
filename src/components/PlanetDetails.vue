384604434964231c77679eb8895c9cec01f45cb0
Alexandre MARTIN <alexandre.martin@diiage.org>
2020-02-10 17:26:52 +0100

    End of workshop

--- src/components/PlanetDetails.vue
+++ src/components/PlanetDetails.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        <h1 v-if="planet.name === undefined">Aucune planète sélectionnée</h1>
-        <h1 v-else> {{ planet.name }} </h1>
+        <h1> {{ planet.name }} </h1>
+        <div>
+            <span>{{planet.climate}}</span><br/>
+            <span v-show="planet.climate === 'frozen'">Attention, le climat est dangereux !</span>
+            <p>{{description}}</p>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name: 'planet-details',
+    computed: {
+        description: function() {
+            return `La planète ${this.planet.name} est une planète de type ${this.planet.terrain}. Son climat est de type : ${this.planet.climate}`;
+        }
+    },
     props: [
         'planet'
     ]
--- src/views/Planets.vue
+++ src/views/Planets.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>Planètes</h1>
+        <div>
+            <label for="planet">Recherchez une planète : </label>
+            <input type="text" name="planet" v-model="searchCriteria">
+        </div>
         <div v-for="(planet, index) of planets" v-bind:key="index">
             <span v-on:click="onPlanetClicked(planet)"> {{ planet.name }} {{ index }} </span>
         </div>
@@ -11,6 +15,7 @@
 <script>
 import axios from 'axios'; 
 import PlanetDetails from '@/components/PlanetDetails.vue';
+import debounce from 'debounce';
 
 export default {
     components: {
@@ -21,16 +26,27 @@ export default {
         axios.get('https://swapi.co/api/planets').then(response => {
             this.planets = response.data.results;
         });
+        this.debouncedSearch = debounce(this.searchPlanets, 400)
+    },
+    watch: {
+        searchCriteria: function() {
+            this.debouncedSearch();
+        }
     },
     data: function() {
         return {
             planets: [],
-            selectedPlanet: undefined
+            selectedPlanet: undefined,
+            searchCriteria: ''
         }
     },
     methods: {
         onPlanetClicked(planet) {
             this.selectedPlanet = planet;
+        },
+        async searchPlanets() {
+            const result = await axios.get(`https://swapi.co/api/planets/?search=${this.searchCriteria}`);
+            this.planets = result.data.results;
         }
     }
 }
